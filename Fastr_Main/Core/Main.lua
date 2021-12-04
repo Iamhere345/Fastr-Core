aed211586c4ecdc0c20204fc671263206762295a
Iamhere345 <95515863+Iamhere345@users.noreply.github.com>
2021-12-04 15:49:57 +1100

    Create Main.lua

---
+++ Fastr_Main/Core/Main.lua
@@ -0,0 +1,20 @@
+--this is the script that calls the parser when a player chats. it's a small script, but without it fastr won't parse commands
+local Fastr = script.Parent.Parent
+local Parser = require(script.Parent.Parser)
+local MiscUtils = require(Fastr.Utils.MiscUtils)
+
+local function TextCommandsSetup(player)
+	
+	player.Chatted:Connect(function(msg)
+		Parser.ParseCmd(player,msg,true)
+	end)
+	
+end
+
+for _,player in pairs(game.Players:GetPlayers()) do --when fastr is loaded play will of already join, so this is to setup the lexer for those players
+	TextCommandsSetup(player)
+end
+
+game.Players.PlayerAdded:Connect(function(player) --some players woll join after fastr has loaded and already looped through the existing players
+	TextCommandsSetup(player)
+end)
