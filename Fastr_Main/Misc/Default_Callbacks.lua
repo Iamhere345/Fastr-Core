34dec1bce7a9ea1854191b03dd89a1e05fd833d8
Iamhere345 <95515863+Iamhere345@users.noreply.github.com>
2021-12-12 09:46:47 +1100

    Update Default_Callbacks.lua

--- Fastr_Main/Misc/Default_Callbacks.lua
+++ Fastr_Main/Misc/Default_Callbacks.lua
@@ -4,11 +4,11 @@ local mps = game:GetService("MarketplaceService")
 --this script contains all of the callbacks made on the client. pretty self-explanitory
 remotes.GetCmdData.OnServerInvoke = function() --this gets every commands metadata and sends them to the client for the :cmds command. this isn't a great place to put it but it's here anyway
 
-	return require(Fastr.Utils.MiscUtils).CompileCommands(Fastr.Core.Commands) --this is gonna keep me up at night
+	return require(Fastr.Utils.MiscUtils).CompileCommands(Fastr.Core.Commands) --don't worry i've checked and this isn't exploitable
 
 end
 
-remotes.PromptPurchase.OnServerEvent:Connect(function(Purchase)
+remotes.PromptPurchase.OnServerEvent:Connect(function(Purchase) --for the menu widgets donate and download
 	
 	local Store = {
 		["Donate100"] = 1225919041,
