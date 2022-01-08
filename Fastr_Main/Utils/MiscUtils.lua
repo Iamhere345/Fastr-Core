eb81ffbeb3fe0bdece5b041ca49031b764b6b66c
Iamhere345 <95515863+Iamhere345@users.noreply.github.com>
2022-01-08 17:05:10 +1100

    Update MiscUtils.lua

--- Fastr_Main/Utils/MiscUtils.lua
+++ Fastr_Main/Utils/MiscUtils.lua
@@ -2,39 +2,45 @@ local MiscUtils = {}
 
 MiscUtils.CompileCommands = function(Root)
 	
-	local compiledTable = {}
+	if script.Parent.Parent.Parent.Parent.Parent.Name == "MainModule" then
+		repeat
+			task.wait()
+		until require(script.Parent.Parent.Parent.Parent.Parent.Name).CoreCommandsEditsCompleted == true
+	end
 	
+	local compiledTable = {}
+
 	for i,v in pairs(Root:GetChildren()) do
 		if v:IsA("ModuleScript") then
-			
+
 			local CommandTable = require(v)
-			
+
 			for i,v in pairs(CommandTable) do
 				compiledTable[string.lower(v.Name)] = v --this is how you can have a command with an uppercase name have fastr still work with that command
 			end
-			
+
 		end
 	end
-	
+
 	return compiledTable
-	
+
 end
 
 MiscUtils.CompileMods = function(CmdTable)
-	
+
 	local fnMods = {
 		["all"] = {"all","me","others","random","randother","team","player"},
 		["standard"] = {"all","me","others",},
 	}
-	
+
 	local function GetFnMods(key,modTable)
-		
+
 		for _,mod in ipairs(fnMods[key]) do
 			table.insert(modTable,mod)	
 		end
-		
+
 	end
-	
+
 	for _,cmd in pairs(CmdTable) do
 		if cmd.Modifyers then
 			for x,mod in pairs(cmd.Modifyers) do
@@ -44,6 +50,18 @@ MiscUtils.CompileMods = function(CmdTable)
 			end
 		end
 	end
+
+end
+
+MiscUtils.DeepCopy = function(t: table)
+
+	local tableCopy = {}
+
+	for k,v in pairs(t) do
+		tableCopy[k] = v
+	end
+	
+	return tableCopy
 	
 end
 
