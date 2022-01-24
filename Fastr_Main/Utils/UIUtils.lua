d7143ecd40cc7bc046c0f48551db10ea38bf99a6
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2022-01-24 16:58:02 +1100

    Delete Fastr_Main directory

--- Fastr_Main/Utils/UIUtils.lua
+++
@@ -1,9 +0,0 @@
-local UIUtils = {}
-
-local Remotes = game.ReplicatedStorage:WaitForChild("Fastr_Remotes")
-
-UIUtils.Notify = function(player,Type,Msg,TopBarMsg,Colour,Duration)
-	Remotes.ShowNotificaiton:FireClient(player,Type,Msg,TopBarMsg,Colour,Duration)
-end
-
-return UIUtils
