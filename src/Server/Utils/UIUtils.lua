7de86a421cfb0d8262384132a0595d7e26d7f5a2
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2022-04-08 15:43:24 +1000

    made the active screengui in Fastr_Client IgnoreGuiInset, fixed typo

--- src/Server/Utils/UIUtils.lua
+++ src/Server/Utils/UIUtils.lua
@@ -3,7 +3,7 @@ local UIUtils = {}
 local Remotes = game.ReplicatedStorage:WaitForChild("Fastr_Remotes")
 
 UIUtils.Notify = function(player, Type, Msg, TopBarMsg, Colour, Duration)
-	Remotes.ShowNotificaiton:FireClient(player, Type, Msg, TopBarMsg, Colour, Duration)
+	Remotes.ShowNotification:FireClient(player, Type, Msg, TopBarMsg, Colour, Duration)
 end
 
 return UIUtils
