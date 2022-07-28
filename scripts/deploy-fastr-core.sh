c2014256fe6f7a8ebd5f950cc08d0fb982f17958
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2022-07-28 13:44:37 +1000

    test

---
+++ scripts/deploy-fastr-core.sh
@@ -0,0 +1,23 @@
+git branch deploy
+
+cd src/Server/
+    rm -rf Resources 
+    rm Core/Commands/*
+cd ../Client
+    rm Active/FlyScript.client.lua
+cd Resources
+    rm FlightControl.rbxmx
+    rm Message.rbxmx
+    rm Notification.rbxmx
+    rm SmallMessage.rbxmx
+cd ../../Remotes
+    rm Fly.model.json
+    rm ShowCountdown.model.json
+    rm ShowNotification.model.json
+    rm ShowSmallMessage.model.json
+    rm ShowMessage.model.json
+
+git commit -am "deploy"
+git push -f fastr-core
+
+git branch -d deploy
